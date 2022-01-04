2bfa84aa493a6626cf6938b96b0f8fd8d4ac700b
Victor Barros <victor.barros@windowslive.com>
2022-01-03 22:05:20 -0300

    custom theme and logo

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-const withPlugins = require("next-compose-plugins");
+const withPlugins = require('next-compose-plugins');
+const withAntdLess = require('next-plugin-antd-less');
 
-module.exports = withPlugins([], {});
+const pluginAntdLess = withAntdLess({
+  lessVarsFilePath: './src/styles/variables.less',
+});
+
+module.exports = withPlugins([[pluginAntdLess]], {});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import { Refine } from '@pankod/refine';
 import routerProvider from '@pankod/refine-nextjs-router';
 
-import '@pankod/refine/dist/styles.min.css';
 import dataProvider from 'src/middleware/dataProvider';
 
 import {
@@ -13,17 +13,49 @@ import {
   ReceiptCreate,
   ReceiptEdit,
 } from '@components';
+import { CopyOutlined } from '@ant-design/icons';
+
+require('antd/dist/antd.less');
 
 const API_TOKEN = process.env.NEXT_PUBLIC_AIRTABLE_API_TOKEN as string;
 const BASE_ID = process.env.NEXT_PUBLIC_AIRTABLE_BASE_ID as string;
+
+const { Link } = routerProvider;
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <Refine
       routerProvider={routerProvider}
       dataProvider={dataProvider(API_TOKEN, BASE_ID)}
+      Title={({ collapsed }) => (
+        <Link to="/" href="/">
+          {collapsed ? (
+            <img
+              src="/logo.png"
+              alt="Refine"
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: '100%',
+                padding: '12px 24px',
+              }}
+            />
+          ) : (
+            <img
+              src="/logo.png"
+              alt="Refine"
+              style={{
+                width: '100%',
+                padding: '12px 24px',
+              }}
+            />
+          )}
+        </Link>
+      )}
       resources={[
         {
+          icon: <CopyOutlined />,
           name: 'receipts',
           list: ReceiptList,
           create: ReceiptCreate,
@@ -34,6 +66,10 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       ]}
       warnWhenUnsavedChanges={true}
     >
+      <Head>
+        <title>OCR Expense Report</title>
+        <link rel="icon" type="image/png" href="/logo.png" />
+      </Head>
       <Component {...pageProps} />
     </Refine>
   );
